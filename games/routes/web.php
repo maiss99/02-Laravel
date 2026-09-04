4390dc96bfe23893cfa6416d321e3a86342d1677
Maissam Aoulad Ali <maissamaouladali@gmail.com>
2026-09-04 12:06:17 +0200

    Read: controller, view en route

--- games/routes/web.php
+++ games/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
