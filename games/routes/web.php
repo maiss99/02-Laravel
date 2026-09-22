de0d70931d30ee32262892eaaeddcbdd05a5bc30
Maissam Aoulad Ali <maissamaouladali@gmail.com>
2026-09-22 12:27:33 +0200

    Les 1/2 laravel 2

--- games/routes/web.php
+++ games/routes/web.php
@@ -1,15 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\GameController;
+use Illuminate\Support\Facades\Route;
+
 Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
 Route::get('games/create', [App\Http\Controllers\GameController::class, 'create']);
 Route::post('games/store', [App\Http\Controllers\GameController::class, 'store']);
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('games/edit/{id}', [App\Http\Controllers\GameController::class, 'edit']);
-Route::post('games/update/{id}', [App\Http\Controllers\GameController::class, 'update']);
-Route::post('games/destroy/{id}', [App\Http\Controllers\GameController::class, 'destroy']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/games/{id}', [GameController::class, 'show']);
+
+require __DIR__.'/auth.php';
+
+
+
+
